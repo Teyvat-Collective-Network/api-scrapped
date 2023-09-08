63a734c92897c30dbe0c423f6ec9c921504c1fd9
hyper-neutrino <alex.yj.liao@gmail.com>
2023-09-08 15:14:22 -0400

    add GET /stats

--- v1/routes/root.js
+++ v1/routes/root.js
@@ -1,6 +1,10 @@
+import uptime from "../lib/uptime.js";
+
 export default function (server, _, done) {
     server.api("GET /stats", async () => {
-        console.log(await server.query(`SELECT COUNT(1) FROM users`));
+        const [{ "COUNT(1)": guildCount }] = await server.query(`SELECT COUNT(1) FROM guilds`);
+        const [{ "COUNT(1)": userCount }] = await server.query(`SELECT COUNT(1) FROM users`);
+        return { guildCount, userCount, uptime: uptime() };
     });
 
     done();
